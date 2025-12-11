085ed77c0001b83cfb2755e349a0212b007a6271
sh11217002-a11y <sh11217002@gmail.com>
2025-12-11 19:46:29 +0900

    Update main.py
    今度こそパラメータ付き

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from datetime import datetime
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index() -> str:
+    return """
+    <html>
+        <head>
+            <title>FastAPI Hello</title>
+        </head>
+        <body>
+            <h1>Welocome</h1>
+            <p>Opne the interactive API does at http://URL/docs.</p>
+        </body>
+    </html>
+    """
+
+
 
 @app.get("/greet{name}")
 async def greet_user(nema: str) -> dict[str, str]:
