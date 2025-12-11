dbf099b7978547e27948911555b4e0459f484fda
sh11217002-a11y <sh11217002@gmail.com>
2025-12-11 19:37:05 +0900

    Update main.py
    挨拶APIパラメータ付きをやろうとしている

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from datetime import datetime
 
-app = FastAPI()
-
-
-@app.get("/", response_class=HTMLResponse)
-async def read_index() -> str:
-    return """
-    <html>
-      <head>
-        <title>FastAPI Hello</title>
-      </head>
-      <body>
-        <h1>Welcome</h1>
-        <p>Open the interactive API docs at http://URL/docs.</p>
-      </body>
-    </html>
-    """
-
-
-@app.get("/hello_world")
-async def read_root() -> dict[str, str]:
-    return {"message": "Good morning world. This is my first API"}
+@app.get("/greet{name}")
+async def greet_user(nema: str) -> dict[str, str]:
+    return {
+        "message": f"Hello, {nema} !",
+        "timestamp": datetime.now().strftime("%H:%M:%S")
+    }
